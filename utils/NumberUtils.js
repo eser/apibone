f0b0ff7b4447386d640bbee63cd3c2821b578a43
Eser Ozvataf <eser@ozvataf.com>
2016-06-21 14:55:58 +0300

    * fixed random number issue.

--- utils/NumberUtils.js
+++ utils/NumberUtils.js
@@ -1,6 +1,6 @@
 class NumberUtils {
     static getRandomInt(min, max) {
-       return Math.floor(Math.random() * (max - min)) + min;
+       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 }
 
