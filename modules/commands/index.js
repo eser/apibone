ae55a7ef5e50d4fc5295eab16c3ead62ea12c1f5
Eser Ozvataf <eser@ozvataf.com>
2016-06-22 19:12:47 +0300

    * commands command fix.

--- modules/commands/index.js
+++ modules/commands/index.js
@@ -1,14 +1,22 @@
 function commandsCommand(argv, session) {
     return Promise.resolve({
-        response: JSON.keys(apiBone.commands),
+        response: Object.keys(apiBone.commands).map((item) => {
+            const command = apiBone.commands[item];
+
+            return {
+                command: command.command,
+                description: command.description,
+                usage: command.usage
+            };
+        }),
         formatText: (result) => {
             for (let command of result) {
-                session.log(command);
+                session.log(`${command.command}: ${command.description}`);
             }
         },
         formatMarkdown: (result) => {
             for (let command of result) {
-                session.log(command);
+                session.log(`*${command.command}*: ${command.description}`);
             }
         },
         formatJson: (result) => { session.log(result); }
@@ -18,6 +26,7 @@ function commandsCommand(argv, session) {
 apiBone.addCommand({
     command: 'commands',
     description: 'List of commands',
+    usage: 'commands',
     demandArgs: 0,
     demandOptions: [ ],
     callback: commandsCommand
