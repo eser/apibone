bbac9343971a20f4fee124b6f0a2f6a80895fb35
Eser Ozvataf <eser@ozvataf.com>
2016-06-22 20:20:24 +0300

    * https fix for speech command.

--- modules/speech/index.js
+++ modules/speech/index.js
@@ -1,11 +1,11 @@
-const http = require('http'),
+const https = require('https'),
     config = require('../../config.js');
 
 function speechCommand(argv, session) {
     const input = encodeURIComponent(argv._.join(' ')),
         lang = argv.lang || 'tr';
 
-    const stream = http.get(`https://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&client=tw-ob&q=${input}&tl=${lang}`);
+    const stream = https.get(`https://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&client=tw-ob&q=${input}&tl=${lang}`);
 
     return Promise.resolve({
         response: {
