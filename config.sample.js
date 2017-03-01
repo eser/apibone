0e6145f9f0a79b84d0eac0bf9e724c7928f340fe
Eser Ozvataf <eser@ozvataf.com>
2017-03-01 23:56:40 +0300

    Slack bots.

--- config.sample.js
+++ config.sample.js
@@ -1,15 +1,22 @@
 module.exports = {
+    defaultCommand: 'def',
     aliases: {
         'bot': 'def bot'
     },
     telegram: {
         botToken: ''
     },
+    slack: {
+        botToken: '',
+        name: 'eserbot'
+    },
     openweathermap: {
         apiKey: ''
     },
     def: {
         '^bot': [ 'i\'m a $0!' ],
+        'praise': [ '$0* is awesome' ],
+        'damn': [ 'damnit $0*' ],
         default: [ 'dunno - $0' ]
     }
 };
